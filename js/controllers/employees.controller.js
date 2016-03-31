8b2705a33f96791c6fbe1dd7274cebe7414583e0
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-31 14:26:35 +0300

    Cleaning and refactoring

--- js/controllers/employees.controller.js
+++ js/controllers/employees.controller.js
@@ -1,7 +1,9 @@
 var employeeApp = angular
                         .module('employeeApp',[])
-                        .controller('employeeController',function($scope,$http){
+                        .controller('employeeController',function($scope,$http,$log){
                           $http.get('employees/employees.json').success(function(data) {
+                            $log.info(data);
                             $scope.employees = data.employees;
+                            $scope.employeeListView = 'views/employeeList.html';
                           });
                         });
