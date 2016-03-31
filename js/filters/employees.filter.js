d50d50466c054b722fcb39c4345bd5a7dee2ff7b
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-31 12:13:35 +0300

    Added custom filter to view

--- js/filters/employees.filter.js
+++ js/filters/employees.filter.js
@@ -1,17 +1,12 @@
-var employeeApp = angular
-                        .module('employeeApp',[])
-                        .filter('gender',function(){
-                          return function(gender){
-                            switch(gender){
-                              case 1:
-                                return "Male";
-                                break;
-                              case 2:
-                                return "Female";
-                                break;
-                              case 3:
-                                return "Undiscolsed";
-                                break;
-                            }
-                          }
-                        });
+employeeApp.filter('gender',function(){
+  return function(gender){
+    switch(gender){
+      case "1":
+        return "Male";
+      case "2":
+        return "Female";
+      case "3":
+        return "Undiscolsed";
+    }
+  }
+});
